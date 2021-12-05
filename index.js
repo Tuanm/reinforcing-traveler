083d971aac150f6954489b7b896f70f1fa1fbf37
vi <tuanm.dev@gmail.com>
2021-12-05 14:09:39 +0700

    rename

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { Agent } from './reinforcement/base';
 import { GridWorld, GridState } from './reinforcement/impl/grid-world';
-import { RandomPolicy, ValueIterationPolicy } from './reinforcement/impl/policies';
+import { RandomPolicy, ValueIterationPolicy } from './reinforcement/impl/grid-world-policies';
 
 function start() {
     // Initialize environment and agent
