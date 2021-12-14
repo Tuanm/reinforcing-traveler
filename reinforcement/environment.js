9aa69be06b3b220af0aa84d467a1b27e6e1641d5
vi <tuanm.dev@gmail.com>
2021-12-14 19:35:53 +0700

    add comment

--- reinforcement/environment.js
+++ reinforcement/environment.js
@@ -19,6 +19,7 @@ export default class Environment {
         this.rewards = rewards;
     }
 
+    // return { nextState, reward }
     response(state, action) {
         throw new NotImplementedError(this.response);
     }
