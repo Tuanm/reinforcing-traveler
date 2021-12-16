a7d0e88268fb5640316ab9e031ac3707b6a5de8e
vi <tuanm.dev@gmail.com>
2021-12-16 19:15:37 +0700

    update UI

--- public/maze/script.js
+++ public/maze/script.js
@@ -5,9 +5,14 @@ const discountFactorInput = document.getElementById('discount-factor-input');
 const explorationRateInput = document.getElementById('exploration-rate-input');
 const maxStepsInput = document.getElementById('max-steps-input');
 const gameStatesContainer = document.getElementById('game-states');
-const gameValuesContainer = document.getElementById('game-values');
 const totalRewardSpanner = document.getElementById('total-reward');
 const nextActionSpanner = document.getElementById('next-action');
+const gameValuesPopup = document.getElementById('game-values');
+const actionUpValueSpanner = document.getElementById('action-up-value');
+const actionLeftValueSpanner = document.getElementById('action-left-value');
+const actionRightValueSpanner = document.getElementById('action-right-value');
+const actionDownValueSpanner = document.getElementById('action-down-value');
+
 
 function createGameBoardRow() {
     const gameBoardRow = document.createElement('div');
@@ -31,16 +36,50 @@ function createGameState(state) {
     return div;
 }
 
-function createGameValue(values) {
-    return undefined;
+function setGameValues(state, policyValues) {
+    const gameValues = {
+        up: undefined,
+        left: undefined,
+        right: undefined,
+        down: undefined
+    };
+    for (const values of policyValues) {
+        if (values[0].state.x === state.x && values[0].state.y) {
+            if (values[0].action === 'UP') gameValues.up = values[1];
+            else if (values[0].action === 'LEFT') gameValues.left = values[1];
+            else if (values[0].action === 'RIGHT') gameValues.right = values[1];
+            else if (values[0].action === 'DOWN') gameValues.down = values[1];
+        }
+        if (gameValues.up !== undefined && gameValues.left !== undefined) {
+            if (gameValues.right !== undefined && gameValues.down !== undefined) {
+                break;
+            }
+        }
+    }
+    actionUpValueSpanner.innerText = gameValues.up;
+    actionLeftValueSpanner.innerText = gameValues.left;
+    actionRightValueSpanner.innerText = gameValues.right;
+    actionDownValueSpanner.innerText = gameValues.down;
 }
 
-function visualizeGameStates(states, currentState) {
+function peekGameStateValues(state, policyValues) {
+    if (state === undefined || policyValues === undefined) return;
+    setGameValues(state, policyValues);
+    const gameState = gameStatesContainer.childNodes[state.x].childNodes[state.y];
+    gameValuesPopup.style.top = gameState.offsetTop - gameState.offsetHeight;
+    gameValuesPopup.style.left = gameState.offsetLeft - gameState.offsetWidth;
+    gameValuesPopup.style.visibility = 'visible';
+}
+
+function visualizeGameStates(states, currentState, policyValues) {
     gameStatesContainer.innerHTML = ''; // remove all child nodes
     for (const stateRow of states) {
         const gameBoardRow = createGameBoardRow();
         for (const state of stateRow) {
             const gameState = createGameState(state);
+            gameState.onclick = function () {
+                peekGameStateValues(state, policyValues);
+            };
             if (state.x == currentState?.x && state.y == currentState?.y) {
                 gameState.style.backgroundColor = 'red';
             }
@@ -57,20 +96,21 @@ const that = io();
 function init() {
     let states;
 
-    that.on('connected', () => {
+    that.on('connected', function () {
         that.emit('fetch-game', undefined);
     });
 
-    that.on('game-fetched', (gameInfo) => {
+    that.on('game-fetched', function (gameInfo) {
         states = gameInfo.environment.states;
         console.log(gameInfo);
         visualizeGameStates(states);
         console.log('game fetched');
     });
 
-    that.on('game-step-fetched', (gameStepInfo) => {
+    that.on('game-step-fetched', function (gameStepInfo) {
         const currentState = gameStepInfo.gameStep.state;
-        visualizeGameStates(states, currentState);
+        const policyValues = gameStepInfo.policyValues;
+        visualizeGameStates(states, currentState, policyValues);
         nextActionSpanner.textContent = gameStepInfo.gameStep.action;
         totalRewardSpanner.textContent = gameStepInfo.gameStep.totalReward;
         console.log(gameStepInfo);
@@ -112,4 +152,8 @@ window.onload = init;
 startButton.onclick = function () {
     if (!that.running) start();
     else stop();
+};
+
+gameValuesPopup.onclick = function () {
+    gameValuesPopup.style.visibility = 'hidden';
 };
